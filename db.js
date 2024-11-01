d36e3aeeba23030f9217f343d11fbb234ebe7e61
bagindaQuadrant <bpulungan@appen.com>
2024-11-01 08:22:40 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,20 +1,20 @@
 // db.js
-const mysql = require("mysql");
+const mysql = require('mysql');
 
 const connection = mysql.createPool({
-  host: "localhost",
-  user: "admin_absen",
-  password: "B@judit0k02018",
-  database: "gbs685_absensi",
+  host: 'localhost',
+  user: 'admin_absen',
+  password: 'B@judit0k02018',
+  database: 'gbs685_absensi',
   connectTimeout: 20000,
 });
 
 connection.getConnection((err, conn) => {
   if (err) {
-    console.error("Error connecting to database:", err);
+    console.error('Error connecting to database:', err);
     return;
   }
-  console.log("Connected to MySQL database!");
+  console.log('Connected to MySQL database!');
 
   // If you need to perform any initial queries, you can do it here using 'connection'
 
